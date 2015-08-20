4e18497ddc890d43644978e736123ccc06037bd0
Jonathan Adam <jonathan_adam@brown.edu>
2015-08-20 10:05:23 -0700

    This works

--- database/index.js
+++ database/index.js
@@ -4,6 +4,7 @@
 var Class = require('uberclass')
 var debug = require('debug')('shepherd:db_coren')
 var _ = require('lodash')
+var async = require('async')
 
 
 
@@ -77,106 +78,11 @@ empty: function(entityType, options, callback)
 
 retryWrap: function(callbackToWrap)
 {
-	return function wrappedExecute()
+	function keepTrying(onSuccess)
 	{
-		var ogExecute = callbackToWrap
-		console.log('ogExecute:', ogExecute)
-		var ogCallback = arguments[arguments.length - 1]
-		function run()
-		{
-			console.log('running')
-			ogExecute.apply(arguments)
-		}
-		function wrappedCallback()
-		{
-			console.log('wrpaper')
-			var err = arguments[0]
-			if (err)
-			{
-
-			console.log('err')
-				setTimeout(wrappedExecute, 500)
-			}
-			else
-			{
-			console.log('win')
-				ogCallback(arguments)
-
-			}
-		}
-		console.log('run')
-		arguments[arguments.length - 1] = wrappedCallback
-		run()
+		async.retry({times:14400, interval: 500} , callbackToWrap, onSuccess)
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// return function retry()
-	// {
-	// 	console.log('ogArguments are', arguments)
-	// 	var ogCallback = arguments[arguments.length -1]
-
-	// 	ogExecute(arguments)
-
-	// 	var err = arguments[0]
-	// 	if (err)
-	// 		return setTimeout(retry(arguments), 500)
-	// 	else
-	// 		ogCallback(arguments)
-		// try
-		// {
-		// 	var retryingCallback = function(err, resp)
-		// 	{
-		// 		if (err)
-		// 		{
-		// 			console.log('oh my god an eerror was found!')
-		// 			throw err
-		// 		}
-		// 		else
-		// 			ogCallback(err, resp)
-		// 	}
-		// 	if (ogArguments.length !== 0)
-		// 		ogArguments[ogArguments.length - 1] = retryingCallback
-		// 	else
-		// 		ogArguments[0] = retryingCallback
-
-		// 	console.log('now ogArguments have turned into ', JSON.stringify(ogArguments))
-		// 	callbackToWrap.apply(this, ogArguments)
-		// }
-		// catch (err)
-		// {
-		// 	console.log('retrying')
-		// 	setTimeout(retry, 500)
-		// }
-	// }
+	return keepTrying
 },
 
 getQuery: function(query)
