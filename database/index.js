ca6e6dc3493355a3cacce00c90aa2739ef65c85d
Jonathan Adam <jonathan_adam@brown.edu>
2015-08-20 12:18:03 -0700

    database now can take a keepTrying option in it

--- database/index.js
+++ database/index.js
@@ -22,14 +22,40 @@ init: function(options, callback){
 	}
 	options = options || {}
 
+	this.keepTrying = options.keepTrying || false
+
 	debug('initialize coren database')
-	corenAPI.init(options, function(err, coren)
+
+	var self = this
+	function initCorenAPI()
 	{
-		if (err)
-			throw err
-		this.coren = coren
-		callback()
-	}.bind(this))
+		console.log('trying to init corenAPI')
+		corenAPI.init(options, function(err, coren)
+		{
+			if (err)
+			{
+				if (options.keepTrying)
+					setTimeout(initCorenAPI, 500)
+				else
+					throw err
+			}
+			else
+			{
+				self.coren = coren
+				callback()
+			}
+		})
+	}
+
+	try{
+		console.log('initing corenapi')
+		initCorenAPI()
+	}
+	catch (err)
+	{
+		console.log('got here')
+		setTimeout(initCorenAPI, 500)
+	}
 },
 create: function(entityType, data, options, callback)
 {
@@ -38,22 +64,64 @@ create: function(entityType, data, options, callback)
 
 find: function(entityType, queryParams, options, callback)
 {
-	return this.coren.find(entityType, queryParams, options, callback)
+	return this.getQuery(this.coren.find(entityType, queryParams, options, callback))
 },
 
 update: function(entityType, queryParams, datas, options, callback)
 {
-	return this.coren.update(entityType, queryParams, datas, options, callback)
+	return this.getQuery(this.coren.update(entityType, queryParams, datas, options, callback))
 },
 
 remove: function(entityType, queryParams, options, callback)
 {
-	return this.coren.remove(entityType, queryParams, options, callback)
+	return this.getQuery(this.coren.remove(entityType, queryParams, options, callback))
 },
 
 empty: function(entityType, options, callback)
 {
 	return this.coren.empty(entityType, options, callback)
+},
+
+retryWrap: function(callbackToWrap)
+{
+
+	var self = this
+
+	function tryRequest(onSuccess, onError)
+	{
+		callbackToWrap(function(err, resp)
+		{
+			if (err)
+				process.nextTick(onError)
+			else
+				onSuccess(err, resp)
+		})
+	}
+
+	function retry(onSuccess, keepTrying)
+	{
+		if (_.isUndefined(keepTrying))
+			keepTrying = self.keepTrying
+
+		console.log('retry is ', keepTrying)
+		if (keepTrying)
+			{
+				tryRequest(onSuccess, function()
+				{
+					console.log('Failed, retrying...')
+					retry(onSuccess, keepTrying)
+				})
+			}
+		else
+			callbackToWrap(onSuccess)
+	}
+	return retry
+},
+
+getQuery: function(query)
+{
+	query.execute = this.retryWrap(query.execute)
+	return query
 }
 
 //end of module
